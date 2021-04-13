3ba091ce4b18497021389a7067d32a7c746c7873
Alex <ata1005@alu.ubu.es>
2021-04-13 16:24:07 +0200

    Implementada ActividadSalidaPuerta.java

--- src/p03/c01/ActividadSalidaPuerta.java
+++ src/p03/c01/ActividadSalidaPuerta.java
@@ -1,8 +1,32 @@
-package p03.c01;
+package src.p03.c01;
 
-public class ActividadSalidaPuerta{
+import java.util.Random;
 
-	//
-	// TODO
-	//
+import src.p03.c01.IParque;
+
+public class ActividadSalidaPuerta implements Runnable{
+
+	private static final int NUMENTRADAS = 20;
+	private IParque parque;
+	private String puerta;
+	
+	public ActividadSalidaPuerta(String puerta, IParque parque) {
+		this.puerta = puerta;
+		this.parque = parque;
+	}
+	
+	@Override
+	public void run() {
+		
+		for(int i = 0; i < NUMENTRADAS; i++) {
+			for(int j = 0; j < 10000; j++) {
+				this.parque.salirDelParque(puerta);
+			}
+			try {
+				Thread.sleep(new Random().nextInt(5)*1000);
+			}catch(InterruptedException e){
+				e.printStackTrace();
+			}
+		}
+	}
 }
